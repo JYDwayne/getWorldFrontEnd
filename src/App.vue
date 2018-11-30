41c202189bee4e1b3405e0dbbbdc397b5b3e26a6
duanjunyan@autohome.com.cn <duanjunyan@autohome.com.cn>
2018-11-30 17:09:59 +0800

    add init code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name : 'app',
   components: {
     HelloWorld
   }
-}
+} 
+
 </script>
 
 <style>
@@ -25,4 +26,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.imgDiv{
+  width: 100px;
+  height: 100px;
+  border:solid 1px red;
+  background-image: url(./assets/logo.png);
+  background-size: 100% 100%;
+}
 </style>
+
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,57 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <span>输入国家名称(模糊查询)</span><input v-model="countryName" type="text" placeholder="请输入国家"><span>每页十条输入页数</span> <input v-model="pageNum" type="text">
+      <div @click="getCountry($event)">
+        点击获取国家列表
+      </div>
+      <div>
+        <div class="depart_line" v-for="(item, index) in countryList" :key="index">
+          <p v-bind:key="indexInner" v-for="(itemInner, indexInner) in item">{{indexInner}}:{{itemInner}}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+
+import qs from 'qs'
+
+let Axios = axios.create({
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+})
+
 export default {
   name: 'HelloWorld',
+  data () {
+    return {
+      countryName: '',
+      pageNum: 1,
+      countryList: []
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+    getCountry ($event) {
+      var that = this
+      Axios.post('/getlist', qs.stringify({
+        countryName: this.countryName,
+        pageNum: this.pageNum - 1
+      })).then((res) => {
+        console.log(res)
+        if (res.data.returncode === 0) {
+          console.log(res.data)
+          that.countryList = res.data.data
+        }
+      })
+    }
   }
 }
 </script>
@@ -54,5 +71,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.depart_line{
+  border-bottom: solid 1px red;
 }
 </style>
